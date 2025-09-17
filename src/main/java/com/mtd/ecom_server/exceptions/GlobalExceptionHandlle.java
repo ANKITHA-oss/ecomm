b88dea437d4f51405697c7bf19b5a953ca044ca9
ANKITHA T <ankithatalapally123@gmail.com>
2025-09-17 14:24:48 +0530

    rsc patch

--- src/main/java/com/mtd/ecom_server/exceptions/GlobalExceptionHandlle.java
+++ src/main/java/com/mtd/ecom_server/exceptions/GlobalExceptionHandlle.java
@@ -1,14 +1,13 @@
 package com.mtd.ecom_server.exceptions;
 
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@//ControllerAdvice
+
 public class GlobalExceptionHandlle {
-	//@ExceptionHandler(ResourceNotFoundException.class)
-//	public ResponseEntity<String>handleNotFound(String msg){
-	//	ResponseEntity.status(HttpStatus.NOT_FOUND).body(msg);
-//	}
-//}
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<String>handleNotFound(String msg){
+		ResponseEntity.status(HttpStatus.NOT_FOUND).body(msg);
+	}
+}
