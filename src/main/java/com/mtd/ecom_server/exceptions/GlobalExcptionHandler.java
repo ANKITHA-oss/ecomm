a03888b6d5ef57b86568b030919eda7c5734d973
ANKITHA T <ankithatalapally123@gmail.com>
2025-09-17 14:45:15 +0530

    Config update

--- src/main/java/com/mtd/ecom_server/exceptions/GlobalExcptionHandler.java
+++ src/main/java/com/mtd/ecom_server/exceptions/GlobalExcptionHandler.java
@@ -2,11 +2,8 @@ package com.mtd.ecom_server.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
 
 public class GlobalExcptionHandler {
 	
